1a3a5c27bfc06ee8cc0b792f7e73406326fc7d82
Matziaz <a01772503@tec.mx>
2026-05-14 19:12:27 -0600

    Update color detection parameters for improved accuracy and performance

--- minichallenge_4/minichallenge_4/color_detector_node.py
+++ minichallenge_4/minichallenge_4/color_detector_node.py
@@ -62,8 +62,8 @@ class ColorDetector(Node):
         self.declare_parameter('green_upper_v', 255)
 
         # Noise/area filtering
-        self.declare_parameter('min_contour_area', 1200.0)
-        self.declare_parameter('min_square_area', 30000.0)
+        self.declare_parameter('min_contour_area', 1000.0)
+        self.declare_parameter('min_square_area', 25000.0)
         self.declare_parameter('dominance_ratio', 1.35)
         self.declare_parameter('morphology_kernel_size', 7)
         self.declare_parameter('morphology_open_iterations', 2)
